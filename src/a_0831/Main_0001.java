ff9183e4518b1cab03a9a4a87ca6bb987d289561
Rohyj <yacheku@naver.com>
2024-08-31 14:12:39 +0900

    초기 프로젝트 생성

---
+++ src/a_0831/Main_0001.java
@@ -0,0 +1,14 @@
+package a_0831;
+
+import java.util.Scanner;
+
+public class Main_0001 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt();
+
+        sc.close();
+        System.out.println(a);
+    }
+}
